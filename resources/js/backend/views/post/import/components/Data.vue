70ec8ef8a5b9f2bb322ea32f8bfd89c8759b259a
mdanikibun <md.anikibun@gmail.com>
2024-05-15 09:02:09 +0700

    fix(ui): update path

--- resources/js/backend/views/post/import/components/Data.vue
+++ resources/js/backend/views/post/import/components/Data.vue
@@ -51,7 +51,7 @@
     <dialog-remove></dialog-remove>
 </template>
 <script>
-import DialogRemove from './removeDialog';
+import DialogRemove from './removeDialog.vue';
 export default {
     name: 'TableLog',
     components: {DialogRemove},
